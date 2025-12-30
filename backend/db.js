20e2b8504809ee2d3a54a48ca840ac2aa1e8d78a
blu3well <david.kiura@gmail.com>
2025-12-30 13:29:34 +0300

    Added SSL block to db.js

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-// This helps us see in the terminal if the connection actually happens
-pool.on("connect", () => {
-  console.log("--- Database Pool Connected ---");
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false, // This line is mandatory for Neon
+  },
 });
 
 module.exports = pool;
